c69256e8f3e71261a1e3d9a6e243d261523bb771
Divyanshu Maithani <div.blackcat@gmail.com>
2016-12-27 23:31:00 +0530

    Add K-ary strings

--- strings-of-n-bits.cpp
+++ strings-of-n-bits.cpp
@@ -24,11 +24,13 @@ Algorithm
 Time Complexity
 ===============
 O(2^n)
+O(k^n)
 
 --------------------------------------------------------------------------------
 
 Output
 ======
+Binary strings:
 000
 100
 010
@@ -37,6 +39,35 @@ Output
 101
 011
 111
+
+K-ary strings:
+000
+100
+200
+010
+110
+210
+020
+120
+220
+001
+101
+201
+011
+111
+211
+021
+121
+221
+002
+102
+202
+012
+112
+212
+022
+122
+222
 
 *******************************************************************************/
 
@@ -56,7 +87,22 @@ void genString(int n) {
     }
 }
 
+void genKString(int n, int k)   {
+    if (n == 0) {
+        printf("%s\n", A);
+    }
+    else    {
+        for (int j = 0; j < k; j++) {
+            A[n - 1] = '0' + j;
+            genKString(n - 1, k);
+        }
+    }
+}
+
 int main()  {
+    printf("Binary strings:\n");
     genString(3);
+    printf("\nK-ary strings:\n");
+    genKString(3, 3);
     return 0;
 }
