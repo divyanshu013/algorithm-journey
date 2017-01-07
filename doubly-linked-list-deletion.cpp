2772785bdbc3630f5b9bb45a352ecfffe85ca616
Divyanshu Maithani <div.blackcat@gmail.com>
2017-01-08 00:36:23 +0530

    Update doubly linked list deletion

--- doubly-linked-list-deletion.cpp
+++ doubly-linked-list-deletion.cpp
@@ -21,10 +21,12 @@ O(n)
 
 Output
 ======
-Buy on day : 0   Sell on day: 3
-Buy on day : 4   Sell on day: 6
+4 -> 3 -> 2 -> 1
+3 -> 2
 
 *******************************************************************************/
+
+#include <stdio.h>
 
 struct DLLNode  {
     int data;
@@ -48,5 +50,76 @@ void insert(DLLNode **head, int data, int position) {
         return;
     }
 
-    
+    DLLNode *p = *head, *q;
+
+    if(position == 0)   {
+        newNode->next = p;
+        p->prev = newNode;
+        *head = newNode;
+    }
+
+    else    {
+        int i = 0;
+        while(p && i < position)   {
+            q = p;
+            p = p->next;
+            ++i;
+        }
+
+        q->next = newNode;
+        newNode->next = p;
+        newNode->prev = q;
+    }
+}
+
+void del(DLLNode **head, int position)  {
+    if(*head == NULL)   {
+        printf("List empty\n");
+        return;
+    }
+
+    DLLNode *p = *head, *q;
+
+    if(position == 0)   {
+        *head = (*head)->next;
+        delete(p);
+    }
+    else    {
+        int i = 0;
+        while(p && i < position)    {
+            q = p;
+            p = p->next;
+            i++;
+        }
+
+        if(!p)  {
+            printf("Position doesn't exists\n");
+            return;
+        }
+        else    {
+            q->next = p->next;
+            delete(p);
+        }
+    }
+}
+
+void printList(DLLNode *head)  {
+    while(head) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("\b\b\b   \n"); // destructive backspace
+}
+
+int main()  {
+    DLLNode *head = getNode(1);
+    insert(&head, 2, 0);
+    insert(&head, 3, 0);
+    insert(&head, 4, 0);
+    printList(head);
+
+    del(&head, 0);
+    del(&head, 2);
+    printList(head);
+    return 0;
 }
