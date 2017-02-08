a5e926f3bb9b2d57b0caebc26401d1a56906eccd
Divyanshu Maithani <div.blackcat@gmail.com>
2017-02-08 16:56:36 +0530

    Reverse level order traversal

--- Trees/reverse-level-order-traversal.cpp
+++ Trees/reverse-level-order-traversal.cpp
@@ -1,15 +1,15 @@
 /*******************************************************************************
 
-Size of tree
-============
+Reverse level order traversal
+=============================
 
-Ref - http://www.geeksforgeeks.org/write-a-c-program-to-calculate-size-of-a-tree/
+Ref - http://www.geeksforgeeks.org/reverse-level-order-traversal/
 
 --------------------------------------------------------------------------------
 
 Problem
 =======
-Finding the size of a binary tree.
+Level order traversal in reverse order of levels
 
 --------------------------------------------------------------------------------
 
@@ -21,11 +21,15 @@ O(n)
 
 Output
 ======
-Size of the tree: 5
+Buy on day : 0   Sell on day: 3
+Buy on day : 4   Sell on day: 6
 
 *******************************************************************************/
 
 #include <stdio.h>
+#include <queue>
+#include <stack>
+using namespace std;
 
 struct Node {
     int data;
@@ -41,12 +45,27 @@ struct Node * getNode(int data)  {
     return newNode;
 }
 
-int getSize(Node *root) {
+void reverseLevel(Node *root)   {
     if(root)    {
-        return getSize(root->left) + getSize(root->right) + 1;
+        queue<Node *> q;
+        stack<Node *> s;
+        q.push(root);
+        while(!q.empty())   {
+            root = q.front();
+            q.pop();
+            s.push(root);
+            if(root->right)
+                q.push(root->right);
+            if(root->left)
+                q.push(root->left);
+        }
+
+        while(!s.empty())   {
+            root = s.top();
+            printf("%d ", root->data);
+            s.pop();
+        }
     }
-    else
-        return 0;
 }
 
 int main()  {
@@ -56,6 +75,8 @@ int main()  {
     root->left->left = getNode(4);
     root->left->right = getNode(5);
 
-    printf("Size of the tree: %d\n", getSize(root));
+    printf("Reverse level order traversal:\n");
+    reverseLevel(root);
+    printf("\n");
     return 0;
 }
