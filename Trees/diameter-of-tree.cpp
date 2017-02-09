2f1ad2e637ba9038a70aa96c07e0bd815c7cc64d
Divyanshu Maithani <div.blackcat@gmail.com>
2017-02-09 19:11:36 +0530

    :star: Diameter of a binary tree

---
+++ Trees/diameter-of-tree.cpp
@@ -0,0 +1,74 @@
+/*******************************************************************************
+
+Diameter of tree
+================
+
+Ref - http://www.geeksforgeeks.org/diameter-of-a-binary-tree/
+
+--------------------------------------------------------------------------------
+
+Problem
+=======
+Diameter of a tree is the number of nodes on the longest path between two leaves
+in the tree (not necessarily through root)
+
+--------------------------------------------------------------------------------
+
+Time Complexity
+===============
+O(n)
+
+--------------------------------------------------------------------------------
+
+Output
+======
+Buy on day : 0   Sell on day: 3
+Buy on day : 4   Sell on day: 6
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+struct Node * getNode(int data)  {
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+
+    return newNode;
+}
+
+int diameter(Node *root, int *height)   {
+    if(root)    {
+        int lh = 0, rh = 0, ld = 0, rd = 0;
+
+        ld = diameter(root->left, &lh);
+        rd = diameter(root->right, &rh);
+
+        *height = max(lh, rh) + 1;
+        return max(lh + rh + 1, max(ld, rd));
+    }
+    else    {
+        *height = 0;
+        return 0;
+    }
+}
+
+int main()  {
+    Node *root = getNode(1);
+    root->left = getNode(2);
+    root->right = getNode(3);
+    root->left->left = getNode(4);
+    root->left->right = getNode(5);
+    root->right->right = getNode(6);
+    int height = 0;
+    printf("Diameter: %d\n", diameter(root, &height));
+    return 0;
+}
