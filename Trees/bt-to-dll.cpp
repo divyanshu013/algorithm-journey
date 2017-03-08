5e72b670628bf5bc67ab13168f111111cded5b4b
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-08 20:12:20 +0530

    :star: BT to DLL alt

--- Trees/bt-to-dll.cpp
+++ Trees/bt-to-dll.cpp
@@ -19,6 +19,21 @@ Problem
     c) Make inorder successor as next of root and root as previous of inorder successor.
 3. Find the leftmost node and return it (the leftmost node is always head of converted DLL).
 
+Alternatively,
+1) Fix Left Pointers: In this step, we change left pointers to point to previous
+    nodes in DLL. The idea is simple, we do inorder traversal of tree. In inorder
+    traversal, we keep track of previous visited node and change left pointer to
+    the previous node.
+
+2) Fix Right Pointers: The above is intuitive and simple. How to change right
+    pointers to point to next node in DLL? The idea is to use left pointers fixed
+    in step 1. We start from the rightmost node in Binary Tree (BT). The rightmost
+    node is the last node in DLL. Since left pointers are changed to point to
+    previous node in DLL, we can linearly traverse the complete DLL using these
+    pointers. The traversal would be from last to first node. While traversing
+    the DLL, we keep track of the previously visited node and change the right
+    pointer to the previous node.
+
 --------------------------------------------------------------------------------
 
 Time Complexity
@@ -30,6 +45,7 @@ O(n)
 Output
 ======
 10 -> 20 -> 30 -> 40 -> 50
+1 -> 2 -> 3 -> 4 -> 5 
 
 *******************************************************************************/
 
@@ -85,6 +101,36 @@ struct Node * toDLL(Node *root) {
     return root;
 }
 
+void fixPrev(Node *root)    {
+    static Node *pre = NULL;
+    if(root)    {
+        fixPrev(root->left);
+        root->left = pre;
+        pre = root;
+        fixPrev(root->right);
+    }
+}
+
+struct Node * fixNext(Node *root)   {
+    Node *pre = NULL;
+    while(root->left)   {
+        root->right = pre;
+        pre = root;
+        root = root->left;
+    }
+    root->right = pre;  // for head node
+
+    return root;
+}
+
+struct Node *toDLLAlt(Node *root)   {
+    fixPrev(root);
+
+    while(root->right)
+        root = root->right;
+    return fixNext(root);
+}
+
 int main()  {
 
     /* BST
@@ -106,5 +152,21 @@ int main()  {
 
     // taking list to the head
     printList(root);
+
+    /* BST
+             4
+           /    \
+          2      5
+         /  \
+        1   3           */
+
+    Node *root2 = getNode(4);
+    root2->left = getNode(2);
+    root2->right = getNode(5);
+    root2->left->left = getNode(1);
+    root2->left->right = getNode(3);
+
+    root2 = toDLLAlt(root2);
+    printList(root2);
     return 0;
 }
